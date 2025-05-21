32b235d20528d0ae165b8e99d7e4ffd7dbdc2177
Chris Bailey <cbailey@rldatix.com>
2025-05-21 11:42:44 -0400

    Update backends

--- djangosaml2_v0181post/backends.py
+++ djangosaml2_v0181post/backends.py
@@ -25,7 +25,7 @@ from django.core.exceptions import (
 from djangosaml2_v0181post.signals import pre_user_save
 
 
-logger = logging.getLogger('djangosaml2')
+logger = logging.getLogger('djangosaml2_v0181post')
 
 
 def get_model(model_path):
@@ -49,7 +49,7 @@ def get_model(model_path):
 
 def get_saml_user_model():
     try:
-        # djangosaml2 custom user model
+        # djangosaml2_v0181post custom user model
         return get_model(settings.SAML_USER_MODEL)
     except AttributeError:
         try:
